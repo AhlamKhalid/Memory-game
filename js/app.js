0abfa93c75ede76826993fb7913118cf5bddbb30
Ahlam <ahlamkarkashan@gmail.com>
2019-09-19 02:02:42 +0300

    create cards

--- js/app.js
+++ js/app.js
@@ -1,7 +1,82 @@
+// create cards
+
+// shape: diamond
+const card_1 = document.createElement("li");
+card_1.className = "card fa fa-diamond open show";
+document.querySelector(".deck").appendChild(card_1);
+
+const card_2 = document.createElement("li");
+card_2.className = "card fa fa-diamond open show";
+document.querySelector(".deck").appendChild(card_2);
+
+// shape: paper-plane
+const card_3 = document.createElement("li");
+card_3.className = "card fa fa-paper-plane-o open show";
+document.querySelector(".deck").appendChild(card_3);
+
+const card_4 = document.createElement("li");
+card_4.className = "card fa fa-paper-plane-o open show";
+document.querySelector(".deck").appendChild(card_4);
+
+// shape: anchor
+const card_5 = document.createElement("li");
+card_5.className = "card fa fa-anchor open show";
+document.querySelector(".deck").appendChild(card_5);
+
+const card_6 = document.createElement("li");
+card_6.className = "card fa fa-anchor open show";
+document.querySelector(".deck").appendChild(card_6);
+
+// shape: bolt
+const card_7 = document.createElement("li");
+card_7.className = "card fa fa-bolt open show";
+document.querySelector(".deck").appendChild(card_7);
+
+const card_8 = document.createElement("li");
+card_8.className = "card fa fa-bolt open show";
+document.querySelector(".deck").appendChild(card_8);
+
+// shape: cube
+const card_9 = document.createElement("li");
+card_9.className = "card fa fa-cube open show";
+document.querySelector(".deck").appendChild(card_9);
+
+const card_10 = document.createElement("li");
+card_10.className = "card fa fa-cube open show";
+document.querySelector(".deck").appendChild(card_10);
+
+// shape: leaf
+const card_11 = document.createElement("li");
+card_11.className = "card fa fa-leaf open show";
+document.querySelector(".deck").appendChild(card_11);
+
+const card_12 = document.createElement("li");
+card_12.className = "card fa fa-leaf open show";
+document.querySelector(".deck").appendChild(card_12);
+
+// shape: bicycle
+const card_13 = document.createElement("li");
+card_13.className = "card fa fa-bicycle open show";
+document.querySelector(".deck").appendChild(card_13);
+
+const card_14 = document.createElement("li");
+card_14.className = "card fa fa-bicycle open show";
+document.querySelector(".deck").appendChild(card_14);
+
+// shape: bomb
+const card_15 = document.createElement("li");
+card_15.className = "card fa fa-bomb open show";
+document.querySelector(".deck").appendChild(card_15);
+
+const card_16 = document.createElement("li");
+card_16.className = "card fa fa-bomb open show";
+document.querySelector(".deck").appendChild(card_16);
+
+/* **************************************************** */
+
 /*
  * Create a list that holds all of your cards
  */
-
 
 /*
  * Display the cards on the page
